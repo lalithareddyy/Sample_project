81568a47c419d76276d2a9bc958a2183b2251fab
lalithareddyy <43957961+lalithareddyy@users.noreply.github.com>
2019-05-09 13:39:06 +0530

    Add files via upload

---
+++ OnlineExamDemo-master/onlineexam/javaRedirectForward.java
@@ -0,0 +1,9 @@
+<%
+	String redirectURL = "http://www.example.com/";
+tring redirectURL = "http://www.example.com/";
+	response.sendRedirect(redirectURL);
+	response.sendForward(redirectURL);
+	return;
+%>
+ 
+ 
